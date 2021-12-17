44daead517f187e08f47319e1a17cc6addb81a68
Henric <henric.bjorkvall@grebban.com>
2021-12-17 14:32:37 +0100

    add react-client

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import App from './App';
-import React from 'react';
 import ReactDOM from 'react-dom';
 
+const client = new QueryClient();
+
 ReactDOM.render(
-  <React.StrictMode>
+  <QueryClientProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </QueryClientProvider>,
   document.getElementById('root')
 );
