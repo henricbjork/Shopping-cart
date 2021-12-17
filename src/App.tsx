17718c3bdf5017a2daeb878ba551dc272f42af93
Henric <henric.bjorkvall@grebban.com>
2021-12-17 16:12:13 +0100

    add basic grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
 import './App.css';
 
+import Item from 'components/Item/Item';
+import { useQuery } from 'react-query';
+import { useState } from 'react';
+
+export type CartItemType = {
+  amount: number;
+  category: string;
+  description: string;
+  id: number;
+  image: string;
+  price: number;
+  title: string;
+};
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch('https://fakestoreapi.com/products')).json();
+};
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts
+  );
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Something went wrong</div>;
+
+  return (
+    <div className="grid grid-cols-4 gap-4 p-6 bg-gray-50">
+      {data?.map((item) => {
+        return (
+          <Item key={item.id} item={item} handleAddToCart={handleAddToCart} />
+        );
+      })}
+    </div>
+  );
 };
 
 export default App;
