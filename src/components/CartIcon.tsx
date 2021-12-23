69b18eae48732aa5255564aa601c78d1ff0afa6b
Henric <henric.bjorkvall@grebban.com>
2021-12-23 15:36:35 +0100

    add store context

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,22 +1,20 @@
+import { CartContext } from 'contexts/CartContext';
 import { FC } from 'react';
+import { useContext } from 'react';
 
 type Props = {
-  amountOfItems: number;
   styles?: string;
   handleClick: () => void;
 };
 
-const CartIcon: FC<Props> = ({
-  amountOfItems = 0,
-  handleClick = () => null,
-  styles,
-}) => {
+const CartIcon: FC<Props> = ({ handleClick = () => null, styles }) => {
+  const { totalItems } = useContext(CartContext);
   return (
     <div
       className={`relative z-10 p-2 inline-block border border-solid ${styles}`}
     >
       <p className="absolute top-[-10px] right-[-10px] bg-red-500 text-white flex justify-center items-center rounded-full w-6 h-6">
-        {amountOfItems}
+        {totalItems}
       </p>
       <button onClick={handleClick}>CART</button>
     </div>
