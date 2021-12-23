da00c66f8d69c8dd5bb921d8a7d63174eebf27d0
Henric <henric.bjorkvall@grebban.com>
2021-12-23 16:21:18 +0100

    remove homepage placeholder for now

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,6 @@ const App = () => {
         setIsOverlayOpen={setIsOverlayOpen}
       />
       <Outlet />
-      <div className="h-screen bg-yellow-500 flex justify-center items-center">
-        Home page goes here
-      </div>
     </CartProvider>
   );
 };
