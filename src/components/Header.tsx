c423a872f19ce1a23dd6e4604ce3e64e79c982bb
Henric <henric.bjorkvall@grebban.com>
2021-12-23 15:36:17 +0100

    add links to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,19 @@
 import CartIcon from 'components/CartIcon';
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 interface Props {
-  amountOfItems: number;
   setIsOverlayOpen: (arg0: boolean) => void;
   isOverlayOpen: boolean;
 }
 
-const Header: FC<Props> = ({
-  amountOfItems,
-  setIsOverlayOpen,
-  isOverlayOpen,
-}) => {
+const Header: FC<Props> = ({ setIsOverlayOpen, isOverlayOpen }) => {
   return (
     <div className="fixed flex justify-between items-center px-6 py-4 border-b shadow-md bg-white w-full z-10">
-      <p>Fakestore</p>
+      <Link to="/">Fakestore</Link>
+      <Link to="/checkout">Checkout</Link>
+      <Link to="/products">Shop</Link>
       <CartIcon
-        amountOfItems={amountOfItems}
         handleClick={() => {
           setIsOverlayOpen(!isOverlayOpen);
         }}
