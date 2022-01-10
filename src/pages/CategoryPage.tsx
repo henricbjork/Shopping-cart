9148991d442e8239a2230ffc250e590df5d028ab
Henric <henric.bjorkvall@grebban.com>
2022-01-10 08:47:48 +0100

    add some styling to loading view

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -4,18 +4,21 @@ import { CartContext } from 'contexts/CartContext';
 import Item from 'components/ProductCard';
 
 const CategoryPage: FC = () => {
-  const { data, isLoading, error, handleAddToCart } = useContext(CartContext);
+  const { data, isLoading, error } = useContext(CartContext);
 
-  if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Something went wrong</div>;
+  if (isLoading)
+    return (
+      <div className="h-screen bg-blue-600 flex justify-center items-center text-white">
+        Loading...
+      </div>
+    );
 
   return (
     <div className="pt-[100px] px-6">
       <div className="grid grid-cols-4 gap-4">
         {data?.map((item) => {
-          return (
-            <Item key={item.id} item={item} handleAddToCart={handleAddToCart} />
-          );
+          return <Item key={item.id} item={item} />;
         })}
       </div>
     </div>
