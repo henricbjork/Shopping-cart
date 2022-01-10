a143b3ec59b285517552d24630c260e17dda03b2
Henric <henric.bjorkvall@grebban.com>
2022-01-10 08:47:07 +0100

    remove carticon when on checkoutpage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,30 @@
+import { Link, useLocation } from 'react-router-dom';
+
 import CartIcon from 'components/CartIcon';
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
 
 interface Props {
   setIsOverlayOpen: (arg0: boolean) => void;
   isOverlayOpen: boolean;
 }
 
-const Header: FC<Props> = ({ setIsOverlayOpen, isOverlayOpen }) => (
-  <div className="fixed flex justify-between items-center px-6 py-4 border-b shadow-md bg-white w-full z-10">
-    <Link to="/">Fakestore</Link>
-    <Link to="/products">Shop</Link>
-    <CartIcon
-      handleClick={() => {
-        setIsOverlayOpen(!isOverlayOpen);
-      }}
-    />
-  </div>
-);
+const Header: FC<Props> = ({ setIsOverlayOpen, isOverlayOpen }) => {
+  const { pathname } = useLocation();
+  const isCheckoutPage = pathname === '/checkout';
+
+  return (
+    <div className="fixed flex justify-between items-center px-6 py-4 border-b shadow-md bg-white w-full z-10">
+      <Link to="/">Fakestore</Link>
+      <Link to="/products">Shop</Link>
+      {!isCheckoutPage && (
+        <CartIcon
+          handleClick={() => {
+            setIsOverlayOpen(!isOverlayOpen);
+          }}
+        />
+      )}
+    </div>
+  );
+};
 
 export default Header;
